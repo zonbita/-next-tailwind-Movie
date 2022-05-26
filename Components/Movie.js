f2cb21809b6595b458643a0aa54c4ac2747448fe
zonbita <50777150+zonbita@users.noreply.github.com>
2022-05-26 20:45:49 +0700

    fix6

--- Components/Movie.js
+++ Components/Movie.js
@@ -4,13 +4,13 @@ import Link from "next/link";
 const Movie = ({ movie }) => {
   return (
     <Link href={`/movie/${movie.id}`} passHref>
-      <div className="bg-white shadow-sm rounded-md cursor-pointer hover:bg-slate-700">
+      {/* <div className="bg-white shadow-sm rounded-md cursor-pointer hover:bg-slate-700">
         <Image src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} loading='eager' layout='responsive' objectFit='fill' width={237} height={271} className="rounded-t-md" alt={movie.title} />
         <div className="px-6 py-2">
           <div className="font-bold text-xl mb-1">{movie.title}</div>
           <p className="text-gray-700 text-base mb-1">{movie.release_date}</p>
         </div>
-      </div>
+      </div> */}
     </Link>
   )
 }
