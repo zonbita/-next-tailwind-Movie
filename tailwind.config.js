2f8a7d50058be0260558ceacdb5ead7a45842895
zonbita <50777150+zonbita@users.noreply.github.com>
2022-05-26 21:40:11 +0700

    fix8

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
     Mode: "jit",
     content: [
       "./pages/**/*.{js,ts,jsx,tsx}",
-      "./components/**/*.{js,ts,jsx,tsx}",
+      "./Components/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
       extend: {},
